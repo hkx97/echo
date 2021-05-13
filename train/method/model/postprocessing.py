63d913470a7c28ff82fd875f33671c83f67d4502
hkx97 <84089763+hkx97@users.noreply.github.com>
2021-05-13 17:56:16 +0800

    add some notes

--- train/method/model/postprocessing.py
+++ train/method/model/postprocessing.py
@@ -2,10 +2,10 @@ import copy
 
 
 def gets_height(outputs,position):
-    '''def 求某一帧的 height'''# 输入siamese 的outputs 和所需要判断的帧的位置，输出帧的height列表
+    # volume-time curve:
+    # from volume-time curve get any position point's heigth
     height = []
-    a = 0
-    b = 0
+    a,b =0, 0
     for i in (position):
         for j in range(i-1):
             if outputs[j,0]>0.5:
@@ -13,16 +13,21 @@ def gets_height(outputs,position):
             else:
                 b += 1
         height.append(a-b)
-        a = 0
-        b = 0
+        a ,b= 0,0
     return height
 
 
 def gets_a4cd_frames(outputs_1):
-    '''分别得到所有可能的a4cs a4cd'''
+    # get all candicate frames of inflection points
+    # if outputs = [[0,1],[0,1],[0,1],[1,0],[1,0],[0,1]]
+    #          *
+    #       .    .    .
+    #    .         *
+    # .  
+    # the * are candicate frames
     a4cd = []
     for i in range(len(outputs_1)-1):
-        if outputs_1[i,0]>0.5 and outputs_1[i+1,0]<0.5:  #a4cd
+        if outputs_1[i,0]>0.5 and outputs_1[i+1,0]<0.5:  #ED
             a4cd.append(i+2)
     return a4cd
 
@@ -30,7 +35,7 @@ def gets_a4cd_frames(outputs_1):
 def gets_a4cs_frames(outputs_1):
     a4cs = []
     for i in range(len(outputs_1)-1):
-        if outputs_1[i,0]<0.5 and outputs_1[i+1,0]>0.5:  #a4cs
+        if outputs_1[i,0]<0.5 and outputs_1[i+1,0]>0.5:  #ES
             a4cs.append(i+2)
     return a4cs
 
@@ -38,10 +43,7 @@ def gets_a4cs_frames(outputs_1):
 def delete_a4cd_frames(outputs_1):
     a4cd = gets_a4cd_frames(outputs_1)
     a4cd_copy = copy.deepcopy(a4cd)
-    a = 0
-    b = 0
-    c = 0
-    d = 0
+    a,b,c,d= 0,0,0,0
     for i in (a4cd):
         t = i-2
         if t < 10:
@@ -55,12 +57,10 @@ def delete_a4cd_frames(outputs_1):
                     c += 1
                 else:
                     d += 1
-            if not (a>b and c<d):   #说明不满足 从a4cd中删除
+            # if a candicate frame not satisfy the trend of up/down, it will be deleted.
+            if not (a>b and c<d):   
                 a4cd_copy.remove(i)
-            a = 0
-            b = 0
-            c = 0
-            d = 0
+            a,b,c,d= 0,0,0,0
         elif t > len(outputs_1)-11:
             for j in range(t-9,t+1):
                 if outputs_1[j,0]>0.5:
@@ -72,12 +72,9 @@ def delete_a4cd_frames(outputs_1):
                     c += 1
                 else:
                     d += 1
-            if not (a>b and c<d):   #说明不满足 从a4cd中删除
+            if not (a>b and c<d):  
                 a4cd_copy.remove(i)
-            a = 0
-            b = 0
-            c = 0
-            d = 0
+            a,b,c,d= 0,0,0,0
         else:
             for j in range(t-9,t+1):
                 if outputs_1[j,0]>0.5:
@@ -89,24 +86,18 @@ def delete_a4cd_frames(outputs_1):
                     c += 1
                 else:
                     d += 1
-            if not (a>b and c<d):   #说明不满足 从a4cd中删除
+            if not (a>b and c<d):   
                 a4cd_copy.remove(i)
-
-            a = 0
-            b = 0
-            c = 0
-            d = 0
-
+            a,b,c,d= 0,0,0,0
     return a4cd_copy
 
 
 def delete_a4cs_frames(outputs_1):
+    # a function for candicate frames selecting
+    # outputs_1 is all candicate frames of inflection points
     a4cs = gets_a4cs_frames(outputs_1)
     a4cs_copy = copy.deepcopy(a4cs)
-    a = 0
-    b = 0
-    c = 0
-    d = 0
+    a,b,c,d= 0,0,0,0
     for i in (a4cs):
         t = i-2
         if t < 10:
@@ -120,16 +111,9 @@ def delete_a4cs_frames(outputs_1):
                     c += 1
                 else:
                     d += 1
-            if not (a<b and c>d):   #说明不满足 从a4cd中删除
+            if not (a<b and c>d):   
                 a4cs_copy.remove(i)
-            a = 0
-            b = 0
-            c = 0
-            d = 0
-
-            
-            
-            
+            a,b,c,d= 0,0,0,0
         elif t > len(outputs_1)-11:
             for j in range(t-9,t+1):
                 if outputs_1[j,0]>0.5:
@@ -141,52 +125,34 @@ def delete_a4cs_frames(outputs_1):
                     c += 1
                 else:
                     d += 1
-            if not (a<b and c>d):   #说明不满足 从a4cd中删除
+            if not (a<b and c>d):   
                 a4cs_copy.remove(i)
-            a = 0
-            b = 0
-            c = 0
-            d = 0
-
-            
-            
-            
+            a,b,c,d= 0,0,0,0
         else:
             for j in range(t-9,t+1):
                 if outputs_1[j,0]>0.5:
                     a += 1
                 else:
                     b += 1
-            
-                
-                
             for j in range(t,t+10):
                 if outputs_1[j,0]>0.5:
                     c += 1
                 else:
                     d += 1
-            if not (a<b and c>d):   #说明不满足 从a4cd中删除
+            if not (a<b and c>d):   
                 a4cs_copy.remove(i)
-
-            a = 0
-            b = 0
-            c = 0
-            d = 0
-            
-    
+            a,b,c,d= 0,0,0,0
     return a4cs_copy
     
-    
-'''滑动窗口'''
+
 def sliding_window(outputs,frames,key):
     '''
-    outputs:siamese 输出的结果
-    a4cd_frames:a4cd 帧列表
-    key:a4cd or a4cs is 1 or 0
-    滑动窗口大小为10，步长为1
-    比较局部极值height，取max值，当存在相等时， 取最后一帧
-    返回剩余帧列表
-
+    outputs:model's output --> a n*2 tensor
+    frames: a list of condicate frames
+    key:ED or ES is 1 or 0
+    sliding window : length is 10 frames , stride is 1 frame
+    the frame with max/min heigth will return as ED/ES
+    if >1frames have max heigth. the last one will be choiced.
     '''
     i = 0
     j = 10
@@ -214,9 +180,6 @@ def sliding_window(outputs,frames,key):
                 index = Height_list.index(min(Height_list))
                 Frames.append(L1[index])
                 L1 = []
-        
-    
-        
         i += 1
         j += 1
         if j == len(outputs)+1:
